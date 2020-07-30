343fe92f9867edcac31f8588a715fed66c5a0e3c
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-07-30 04:07:37 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,13 @@
 def unsafe?(speed)
-
+  if speed  > 60 || speed < 40
+    return true
+  else
+    return FALSE
+  end
 end
 
-
-
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : FALSE
 end
 	
 
